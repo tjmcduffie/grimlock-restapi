76f3fca322652850754d37bc34a0d13b3e90f538
HUGE | Timothy McDuffie <tmcduffie@hugeinc.com>
2013-12-11 00:11:54 -0500

    removed linkedin config info - this is now in the client side app and irrelevant here

--- config.js
+++ config.js
@@ -22,13 +22,3 @@ exports.database = {
     name: "grimlock-prod",
   }
 };
-
-// Linkedin app details
-exports.linkedin = {
-  company: 'Self Employed',
-  application_name: 'timmcduffie.com api',
-  api_key: '0m2ewzmu2sn7',
-  secret_key: 'O0eLN3SVZLz0L28s',
-  oauth_user_token: 'cd983a91-8097-4342-837f-728557581989',
-  oauth_user_secret: '71a88231-706f-4488-bc7b-f2b8a612ac0b'
-};
