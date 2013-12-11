b7e427f8300bd1cbd98740b61b47a4d333cd6034
HUGE | Timothy McDuffie <tmcduffie@hugeinc.com>
2013-12-11 12:12:54 -0500

    fixed line spacing

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -1,10 +1,13 @@
 var util = require(process.cwd() + '/lib/util');
 var Controller = require(process.cwd() + '/lib/Controller');
 
+
+
 var ProfileController = function() {
   util.base(this);
 };
 util.inherits(ProfileController, Controller);
+
 
 ProfileController.prototype.setRoutes = function() {
   this.addRoute('post', '/profile', this.postProfile);
@@ -13,11 +16,13 @@ ProfileController.prototype.setRoutes = function() {
   this.addRoute('del', '/profile', this.deleteProfile, this);
 };
 
+
 ProfileController.prototype.postProfile = function create(req, res, next) {
   // do some creation stuff here
   this.respondWith(res, { status: 201, content: [{ foo: 'bar' }] });
   return next();
 };
+
 
 ProfileController.prototype.getProfileByName = function(req, res, next) {
   // do some retrieval here
@@ -25,11 +30,13 @@ ProfileController.prototype.getProfileByName = function(req, res, next) {
   return next();
 };
 
+
 ProfileController.prototype.putProfile = function(req, res, next) {
   // do some retrieval here
   this.respondWith(res, { status: 200, content: [{ foo: 'bar' }] });
   return next();
 };
+
 
 ProfileController.prototype.deleteProfile = function(req, res, next) {
   // do some retrieval here
