5e90c5ae723d4b519e36c189570ceedc89002daa
HUGE | Timothy McDuffie <tmcduffie@hugeinc.com>
2013-12-22 11:36:14 -0500

    updated update method to allow the PUT request to update a mongo record. added a flattenObj method to assist with updates

--- controllers/User.js
+++ controllers/User.js
@@ -14,9 +14,9 @@ util.inherits(UserController, Controller);
 
 UserController.prototype.setRoutes = function() {
   this.addRoute('post', '/user', this.postUser, true);
-  this.addRoute('get', '/user/:name', this.getUserByName);
-  this.addRoute('put', '/user/:name', this.putUser, true);
-  this.addRoute('del', '/user/:name', this.deleteUser, true);
+  this.addRoute('get', '/user/:reqFullName', this.getUserByName);
+  this.addRoute('put', '/user/:reqFullName', this.putUser, true);
+  this.addRoute('del', '/user/:reqFullName', this.deleteUser, true);
 };
 
 
@@ -24,9 +24,9 @@ UserController.prototype.postUser = function create(req, res, next) {
   // do some creation stuff here
   var data = {
     email: ParamCleaner.clean(req.params.email),
-    firstName: ParamCleaner.clean(req.params.firstname),
-    fullName: ParamCleaner.clean(req.params.fullname),
-    lastName: ParamCleaner.clean(req.params.lastname),
+    firstName: ParamCleaner.clean(req.params.firstName),
+    fullName: ParamCleaner.clean(req.params.fullName),
+    lastName: ParamCleaner.clean(req.params.lastName),
     lid: ParamCleaner.clean(req.params.lid)
   };
   var user = [];
@@ -41,9 +41,9 @@ UserController.prototype.postUser = function create(req, res, next) {
 
 
 UserController.prototype.getUserByName = function(req, res, next) {
-  var name = ParamCleaner.clean(req.params.name);
+  var reqFullName = ParamCleaner.clean(req.params.reqFullName);
   var users = [];
-  this.model.findOneByFullName(name, function (result) {
+  this.model.findOneByFullName(reqFullName, function (result) {
     if (result) {
       users.push(result);
     }
@@ -55,13 +55,21 @@ UserController.prototype.getUserByName = function(req, res, next) {
 
 UserController.prototype.putUser = function(req, res, next) {
   // do some retrieval here
+  var propKeys = ['firstName', 'lastName', 'fullName', 'email', 'lid'];
+  var propsToSet = {};
   var query = {
-    fulName: ParamCleaner.clean(req.params.name)
+    fullName: ParamCleaner.clean(req.params.reqFullName)
   };
-  var properties = this.flattenProperties_();
-  this.model.update(query, properties, function(results) {
-    this.respondWith(res, { status: 200, content: [results] });
+
+  propKeys.forEach(function(elem) {
+    if (req.params[elem]) {
+      propsToSet[elem] = ParamCleaner.clean(req.params[elem]);
+    }
   });
+
+  this.model.update(query, { $set: util.flattenObject(propsToSet) }, function(results) {
+    this.respondWith(res, { status: 200, content: [results] });
+  }.bind(this));
   return next();
 };
 
@@ -69,7 +77,7 @@ UserController.prototype.putUser = function(req, res, next) {
 UserController.prototype.deleteUser = function(req, res, next) {
   // do some retrieval here
   var params = {
-    fullName: ParamCleaner.clean(req.params.name)
+    fullName: ParamCleaner.clean(req.params.reqFullName)
   };
   this.model.del(params, function(numberOfRemovedResults) {
     this.respondWith(res, { status: 200 , content: numberOfRemovedResults});
