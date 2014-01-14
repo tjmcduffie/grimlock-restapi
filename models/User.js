d6639ac954333bcea1a6aecefb2c7783c79a9400
HUGE | Timothy McDuffie <tmcduffie@hugeinc.com>
2014-01-14 18:43:08 -0500

    nitial changes to integrate mongoose

--- models/User.js
+++ models/User.js
@@ -1,93 +1,61 @@
+// var UserSchema = require(process.cwd() + '/db/schema/User');
 var util = require(process.cwd() + '/lib/util');
 var Model = require(process.cwd() + '/lib/Model');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
+
+
+// var Model = require(process.cwd() + '/lib/Model');
 
 
 var UserModel = function() {
-  this.collectionName = 'users';
+  this.name = 'User';
 
-  util.base(this);
+  // util.base(this, 'User', User.schema, UserModel.schemaMethods);
+  var schema = UserModel.createSchema_(UserModel.schema);
+  return mongoose.model('User', schema);
+
 };
 util.inherits(UserModel, Model);
 
+UserModel.schema = {
+  email: String,
+  name: {
+    first: {type: String, index: true},
+    last: {type: String, index: true}
+  },
+  lid: Number
+};
 
-UserModel.prototype.findAll = function(callback) {
-  try {
-    util.base(this, 'findAll', callback);
-  } catch (e) {
-    console.log(e);
+UserModel.schema.VirtualProperties = {
+  'name.full': {
+    set: function(name) {
+      var split = name.split(' ');
+      this.name.first = split[0];
+      this.name.last = split[1];
+    }
   }
 };
 
+UserModel.schema.InstanceMethods = {};
 
-UserModel.prototype.findOneById = function(id, callback) {
-  try {
-    util.base(this, 'findOneById', id, callback);
-  } catch (e) {
-    console.log(e);
+UserModel.schema.StaticMethods = {};
+
+UserModel.createSchema_ = function(Model) {
+  var schema = new Schema(Model.schema, {autoIndex: false});
+  for (var method in Model.schemaInstanceMethods) {
+    if (Model.schemaInstanceMethods.hasOwnProperty(method)) {
+      schema.methods[method] = Model.schemaInstanceMethods[method];
+    }
   }
-};
-
-
-UserModel.prototype.findOneByFullName = function(name, callback) {
-  try {
-    var props = {
-      fullName: name
-    };
-    this.findOneByProps(props, callback);
-  } catch (e) {
-    console.error(e);
+  for (var method in Model.schemaStaticMethods) {
+    if (Model.schemaStaticMethods.hasOwnProperty(method)) {
+      schema.statics[method] = Model.schemaStaticMethods[method];
+    }
   }
+  return schema;
 };
 
-
-UserModel.prototype.findOneByLinkedInId = function(lid, callback) {
-  try {
-    var props = {
-      lid: lid
-    };
-    this.findOneByProps(props, callback);
-  } catch (e) {
-    console.error(e);
-  }
-};
-
-
-UserModel.prototype.update = function(query, properties, callback) {
-  try {
-    util.base(this, 'update', query, properties, callback);
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-
-UserModel.prototype.del = function(params, callback) {
-  try {
-    util.base(this, 'del', params, callback);
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-
-UserModel.prototype.insert = function(user, callback) {
-  try {
-    util.base(this, 'insert', user, callback);
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-
-
-UserModelData = function(data) {
-  this._id = data._id;
-  this.firstName = data.firstname || undefined;
-  this.lastName = data.lastname || undefined;
-  this.fullName = data.fullname || undefined;
-  this.lid = data.lid || undefined;
-  this.email = data.email || undefined;
-};
 
 module.exports = UserModel;
