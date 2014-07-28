f4f5e767d7db25a98373cc00a4b35fafa963bc60
HUGE | Timothy McDuffie <tmcduffie@hugeinc.com>
2014-07-28 11:51:55 -0400

    added missing specs and resolved JShint issues

--- spec/controllers/profile_spec.js
+++ spec/controllers/profile_spec.js
@@ -17,7 +17,7 @@ describe('The Profile Controller', function() {
       ]
     }
   };
-  var next = function() {};
+  var referenceDocuments = 'projects employments educations user';
 
   beforeEach(function() {
     req = {
@@ -26,8 +26,7 @@ describe('The Profile Controller', function() {
         id: '5377dacb984187ac6bdf3c4f'
       }
     };
-    res = { json: function() {} };
-    spyOn(res, 'json');
+    res = jasmine.createSpyObj('res', ['json']);
     spyOn(crud, 'create');
     spyOn(crud, 'readOne');
     spyOn(crud, 'readMany');
@@ -37,35 +36,63 @@ describe('The Profile Controller', function() {
 
   describe('has a createProfile method', function() {
     it('that calls through to CRUD.create', function() {
-      ProfileController.createProfile(req, res, next);
+      ProfileController.createProfile(req, res);
       expect(crud.create).toHaveBeenCalled();
     });
   });
 
   describe('has a readOneProfile method', function() {
     it('that calls through to CRUD.readOne', function() {
-      ProfileController.readOneProfile(req, res, next);
+      ProfileController.readOneProfile(req, res);
       expect(crud.readOne).toHaveBeenCalled();
+    });
+    it('that doesn\'t populate reference documents by default', function() {
+      ProfileController.readOneProfile(req, res);
+      expect(crud.readOne.mostRecentCall.args[3].populate).toBe(undefined);
+    });
+    it('that populates reference documents when the detail param is set to full', function() {
+      req.params.detail = 'full';
+      ProfileController.readOneProfile(req, res);
+      expect(crud.readOne.mostRecentCall.args[3].populate).toBe(referenceDocuments);
     });
   });
 
   describe('has a readManyProfiles method', function() {
     it('that calls through to CRUD.readMany', function() {
-      ProfileController.readManyProfiles(req, res, next);
+      ProfileController.readManyProfiles(req, res);
       expect(crud.readMany).toHaveBeenCalled();
+    });
+    it('that doesn\'t populate reference documents by default', function() {
+      ProfileController.readManyProfiles(req, res);
+      expect(crud.readMany.mostRecentCall.args[2].populate).toBe(undefined);
+    });
+    it('that populates reference documents when the detail param is set to full', function() {
+      req.params.detail = 'full';
+      ProfileController.readManyProfiles(req, res);
+      expect(crud.readMany.mostRecentCall.args[2].populate).toBe(referenceDocuments);
+    });
+    it('that requests the first page of results when no page number is provided', function() {
+      ProfileController.readManyProfiles(req, res);
+      expect(crud.readMany.mostRecentCall.args[2].skip).toBe(0);
+    });
+    it('that requests the second page of results when page number 2 is provided', function() {
+      req.params.page = 2;
+      ProfileController.readManyProfiles(req, res);
+      expect(crud.readMany.mostRecentCall.args[2].skip).toBe((req.params.page - 1) *
+          crud.readMany.mostRecentCall.args[2].limit);
     });
   });
 
   describe('has a updateProfile method', function() {
     it('that calls through to CRUD.update', function() {
-      ProfileController.updateProfile(req, res, next);
+      ProfileController.updateProfile(req, res);
       expect(crud.update).toHaveBeenCalled();
     });
   });
 
   describe('has a deleteProfile method', function() {
     it('that calls through to CRUD.del', function() {
-      ProfileController.deleteProfile(req, res, next);
+      ProfileController.deleteProfile(req, res);
       expect(crud.del).toHaveBeenCalled();
     });
   });
--- spec/controllers/user_spec.js
+++ spec/controllers/user_spec.js
@@ -20,7 +20,7 @@ describe('The User Controller', function() {
       'formatted': 'tester mcdoofus'
     }
   };
-  var next = function() {};
+  var referenceDocuments = 'profile';
 
   beforeEach(function() {
     req = {
@@ -29,8 +29,7 @@ describe('The User Controller', function() {
         id: fakeUser.id
       }
     };
-    res = { json: function() {} };
-    spyOn(res, 'json');
+    res = jasmine.createSpyObj('res', ['json']);
     spyOn(crud, 'create');
     spyOn(crud, 'readOne');
     spyOn(crud, 'readMany');
@@ -40,35 +39,55 @@ describe('The User Controller', function() {
 
   describe('has a createUser method', function() {
     it('that calls through to CRUD.create', function() {
-      UserController.createUser(req, res, next);
+      UserController.createUser(req, res);
       expect(crud.create).toHaveBeenCalled();
     });
   });
 
   describe('has a readOneUser method', function() {
     it('that calls through to CRUD.readOne', function() {
-      UserController.readOneUser(req, res, next);
+      UserController.readOneUser(req, res);
       expect(crud.readOne).toHaveBeenCalled();
+      expect(crud.readOne.mostRecentCall.args[3].populate).toBe(referenceDocuments);
     });
   });
 
   describe('has a readManyUsers method', function() {
     it('that calls through to CRUD.readMany', function() {
-      UserController.readManyUsers(req, res, next);
+      UserController.readManyUsers(req, res);
       expect(crud.readMany).toHaveBeenCalled();
+    });
+    it('that doesn\'t populate reference documents by default', function() {
+      UserController.readManyUsers(req, res);
+      expect(crud.readMany.mostRecentCall.args[2].populate).toBe(undefined);
+    });
+    it('that populates reference documents when the detail param is set to full', function() {
+      req.params.detail = 'full';
+      UserController.readManyUsers(req, res);
+      expect(crud.readMany.mostRecentCall.args[2].populate).toBe(referenceDocuments);
+    });
+    it('that requests the first page of results when no page number is provided', function() {
+      UserController.readManyUsers(req, res);
+      expect(crud.readMany.mostRecentCall.args[2].skip).toBe(0);
+    });
+    it('that requests the second page of results when page number 2 is provided', function() {
+      req.params.page = 2;
+      UserController.readManyUsers(req, res);
+      expect(crud.readMany.mostRecentCall.args[2].skip).toBe((req.params.page - 1) *
+          crud.readMany.mostRecentCall.args[2].limit);
     });
   });
 
   describe('has a updateUser method', function() {
     it('that calls through to CRUD.update', function() {
-      UserController.updateUser(req, res, next);
+      UserController.updateUser(req, res);
       expect(crud.update).toHaveBeenCalled();
     });
   });
 
   describe('has a deleteUser method', function() {
     it('that calls through to CRUD.del', function() {
-      UserController.deleteUser(req, res, next);
+      UserController.deleteUser(req, res);
       expect(crud.del).toHaveBeenCalled();
     });
   });
