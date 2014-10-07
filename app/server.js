b02d60b23e0231388542a1d2ced07a25cedf7ea8
HUGE | Timothy McDuffie <tmcduffie@hugeinc.com>
2014-10-07 16:35:14 -0400

    added 404 middleware for reponse normalization

--- app/server.js
+++ app/server.js
@@ -1,12 +1,13 @@
 /** REQUIREMENTS */
 var express = require('express');
+var Response = require('./lib/Response');
 
 // environment & config
 var env = process.env.NODE_ENV || 'dev';
 var config = require('./config/app')[env];
 
 /** connect to mongo */
-require('mongo-connector')();
+require('./mongo-connector')();
 
 /** Express */
 // create server
@@ -18,6 +19,24 @@ server.use(require('compression')());
 
 // apply routes
 require('./config/routes')(server);
+
+server.use(function(req, res, next) {
+  // console.log(res);
+  if (!req.route) {
+    var err = new Error('not found');
+    err.status = 404;
+    err.msg = 'not found';
+  }
+
+  next(err);
+});
+
+// Error pages
+server.use(function(err, req, res, next) {
+  console.log('calling error middleware');
+  var response = new Response(err, res.data);
+  res.json(response.getCode(), response.getData());
+});
 
 // listen to the supplied port
 server.listen(config.ports.server, function() {
